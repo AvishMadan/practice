620728b94dfda6dd552a94c354a5988861456c91
Avish Madan <avishmadan@gmail.com>
2018-08-22 20:24:09 +0530

    yeah

---
+++ NewJenkins3/src/main/java/MyJenkins/NewJenkins/TestScript.java
@@ -0,0 +1,17 @@
+package MyJenkins.NewJenkins;
+
+import org.testng.annotations.Test;
+
+public class TestScript {
+	//avish
+	@Test
+	public void avish()
+	{
+		
+		System.out.println("firstTest on jenkins");
+		
+		
+		
+	}
+
+}
